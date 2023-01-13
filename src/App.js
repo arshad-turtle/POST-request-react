7c7ee00b3a8c02cb14bb3f879243e8d7ad59e7ab
arshad-turtle <arshad.bagwan@turtlemint.com>
2023-01-13 21:57:22 +0530

    rest api post request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class  App extends React.Component{
+  constructor(){
+  super()
+  this.state = {
+    name:"",
+    salary:"",
+    age:""
+  }
+  }
+  submit()
+  {
+    let url ="https://dummy.restapiexample.com/api/v1/create"
+    let data = this.state
+    fetch(url,{
+      method: 'POST',
+      headers: {
+      "Content-Type" : "application/json",
+      "Accept":"application/json"
+      },
+      body: JSON.stringify(data)
+    }).then( (result) => {
+      result.json().then((resp) => {
+        console.warn("resp ",resp);
+      })
+    })
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+
+        <h1> simple rest api </h1>
+        <input type="text" name ="name" value={this.state.name} onChange={(data) => {  this.setState({name:data.target.value})} } /> <br /> <br/>
+        <input type="text" name ="salary" value={this.state.salary} onChange={(data) => {  this.setState({salary:data.target.value})}} /> <br /> <br/>
+        <input type="text" name ="age" value={this.state.age} onChange={ (data) => { this.setState({age: data.target.value})} } /> <br /> <br/>
+        <button onClick={() => {this.submit()}}>Submit Data</button>
+
+      </div>
+    )
+  }
 }
-
-export default App;
